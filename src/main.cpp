c762a6594ff8197678767f05b3e1a16b4472f3b9
Pdante1897 <66376784+Pdante1897@users.noreply.github.com>
2021-08-05 22:58:46 -0600

    Inicial

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+void hola_mundo(){
+    cout<<"que pex"<<endl;
+}
+
+
+int main()
+{
+    cout<<"holamundo"<<endl;
+    while (true)
+    {
+        
+        hola_mundo();
+    }
+    
+    return 0;
+}
